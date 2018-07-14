68d41f51bbe24e70fa278538859a703ceee6eb0d
NahlaEssam <nahlaessam@gmail.com>
2018-07-14 12:16:49 +0200

    add server code

--- shared.R
+++ shared.R
@@ -69,6 +69,85 @@ page <- function(...){
   )
 }
 
-showLocalModal <- function(){
+modalTitle <-  function(replace , titleMessage , ...){
+  if(replace){
+    tagList(
+     ...
+    )
+  }
+  else{
+    title = 'Video Tutorial';
+    if(titleMessage!= ""){
+      title = titleMessage
+    }
+   
+    tagList(
+      span(title),
+      modalButton("")
+    )
+  }
   
+}
+modalContent <- function(replace, content , stepsTitle , steps, ...){
+  if(replace){
+   div( ...)  
+  }
+  else{
+  
+    title <- "STEPS"
+    if(stepsTitle != ""){
+      title <- stepsTitle
+    }
+    localSteps <- array()
+    if(steps != ""){
+      localSteps <- steps
+    }
+    else{
+      steps <- c("Please Enter the requested values to apply the reflection" ,"Triangle consists of 3 points (A,B,C) Please Enter Coordinates below " ,"Point A (x1,y1)   Point B (x2,y2)   Point C (x3,y3)" )
+    }
+    
+    div(class="modalContent",
+        div(class="video-Content margin-bottom-20",
+            
+            lapply(1:(length(content)), function(i) {
+              if(i %% 3 == 0){
+                tags$video(src = content[i-2], type = content[i-1], controls = content[i])
+              }
+            })
+        ),
+        div(class="steps",
+            h4(title),
+            
+            tags$ol(class="stepsList",
+                    
+                    
+                    lapply(1:(length(steps)), function(i) {
+                      
+                      tags$li(steps[i])
+                      
+                    })
+            )
+        )
+    )
+  }
+  
+  
+}
+modalDialogLocal <- function(header , body , rEasyClose, easyClose , rFooter , footer ){
+   localeasyClose <- TRUE
+   localfooter <- NULL
+   if(rEasyClose){
+     localeasyClose <- easyClose
+   }
+   
+   if(rFooter){
+     localfooter <- footer
+   }
+   
+   modalDialog(
+     title = header,
+     body,
+     easyClose = localeasyClose,
+     footer = localfooter
+   )
 }
